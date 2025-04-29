3b4240f753f3edb1e7bb55f158b7ca21100d9e73
meowbiceps <marcoozfrend@gmail.com>
2025-04-29 22:16:27 +0300

    homework

---
+++ jsBasics/task2.js
@@ -0,0 +1,7 @@
+let name1 = "Кирило"
+let name2 = "Владислав"
+let greetingContact = "Доброго дня " + name1 + " і " + name2 + "!";
+console.log(greetingContact) 
+
+let greetingTempLate = `Привіт, ${name1} та ${name2}!`;
+console.log(greetingTempLate);
