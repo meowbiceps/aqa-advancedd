3828becd237e53367cf258ce704c07a2615d8c8d
meowbiceps <marcoozfrend@gmail.com>
2025-05-19 17:50:22 +0300

    task6

---
+++ homework_8/task6.js
@@ -0,0 +1,7 @@
+const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
+
+const sortedNumbers = [...numbersList].sort((a, b) => a - b);
+
+console.log("Оригінальний місив:", numbersList);
+console.log("Відсортований масив:", sortedNumbers);
+
