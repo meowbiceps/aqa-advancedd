50939d3f3a8dadf5982c74b10a67071a1ba2925e
meowbiceps <marcoozfrend@gmail.com>
2025-05-31 15:20:09 +0300

    homework

---
+++ homework_11/task1.js
@@ -0,0 +1,7 @@
+function saySmth(message, time) {
+    setTimeout(function() {
+        console.log(message);
+      }, time);
+}
+
+saySmth("homework", 2000);
---
+++ homework_11/task3.js
@@ -0,0 +1,36 @@
+export async function getTodo() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await response.json();
+    return data;
+}
+
+export async function getUser() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+    const data = await response.json();
+    return data;
+
+}
+
+export function promiseAll(promise1, promise2) {
+    Promise.all([promise1, promise2])
+    .then (([todo, user]) => {
+        console.log("Результат Promise.all:");
+        console.log("Todo", todo);
+        console.log("User", user);
+    })
+        .catch(error => {
+            console.error("Помилка у Promise", error);
+        });
+}
+
+
+export function promiseRace(promise1, promise2) {
+    Promise.race([promise1, promise2])
+        .then(result => {
+            console.log("Результат Promise.race:", result); 
+        })
+        .catch(error => {
+            console.error("Помилка у Promise.race:", error);
+        });
+
+}
